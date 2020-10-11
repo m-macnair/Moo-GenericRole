cbb9a15366057d4e2caaaaef9a783da492cda6c0
m <mmacnair.org>
2020-10-11 19:24:56 +0100

    new api helper

--- lib/Moo/GenericRole/Web.pm
+++ lib/Moo/GenericRole/Web.pm
@@ -1,7 +1,7 @@
 #ABSTRACT : use CGI sensibly
 package Moo::GenericRole::Web;
-our $VERSION = 'v1.0.7';
-##~ DIGEST : 279ab5614134c52e781ca4ae6579ee3f
+our $VERSION = 'v1.1.1';
+##~ DIGEST : 886138a7764319f904359be30fcefd4c
 use Moo::Role;
 with qw/Moo::GenericRole/;
 use Carp;
@@ -26,4 +26,38 @@ sub json_req {
 	return $self->json->encode( $req_data );
 
 }
+
+sub sub_as_json_api {
+	my ( $self, $sub, $input ) = @_;
+	$input ||= $self->cgi->param( 'POSTDATA' );
+
+	# 	die $input;
+	my $response_struct;
+	if ( $input ) {
+		my $req_struct = $self->json->decode( $input );
+
+		#try/catch
+		$response_struct = &$sub( $req_struct );
+		warn ref( $response_struct );
+		if ( ref( $response_struct ) eq 'HASH' ) {
+
+			#de nada
+			warn "all good";
+		} else {
+			$response_struct = {fail => 'unhandled error',};
+		}
+	} else {
+		$response_struct = {fail => 'empty request'};
+	}
+	use Data::Dumper;
+	warn "restruct : " . Dumper( $response_struct );
+
+	#try/catch
+	my $response_string = $self->json->encode( $response_struct );
+
+	print $self->cgi->header();
+	print $response_string;
+
+}
+
 1;
